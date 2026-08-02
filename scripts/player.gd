3e9757dcfe47571c005214ab4aa559086bb1656c
Ilhan Dieji <ilhan.dieji@gmail.com>
2026-08-03 01:11:23 +0330

    bird flying and game start function implemented as well as ground and bird falling anims

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,35 @@
 extends RigidBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var started := false
+var flap_force := -340.0
+var flap_angular_force := -8.0
+var max_rotation_up := -30.0
+var max_rotation_down := 90.0
+var falling_angular_velocity := 5.0
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("flap"):
+		if !started:
+			start_game()
+		flap()
+		
+	if rotation_degrees <= max_rotation_up:
+		rotation_degrees = max_rotation_up
+		angular_velocity = 0.0
+		
+	if linear_velocity.y > 0.0:
+		if rotation_degrees <= max_rotation_down:
+			angular_velocity = falling_angular_velocity
+		else:
+			angular_velocity = 0.0
+
+func start_game() -> void:
+	started = true
+	gravity_scale = 1.0
+	
+func flap() -> void:
+	linear_velocity.y = flap_force
+	angular_velocity = flap_angular_force
+	animation_player.play("flap")
