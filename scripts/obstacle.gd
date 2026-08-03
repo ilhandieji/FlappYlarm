2a6da996950ae427c1764cfa96ffdbee4d0a0fd6
Ilhan Dieji <ilhan.dieji@gmail.com>
2026-08-03 16:54:47 +0330

    Pipe (Obstacle) is moving and detecting the touch with Player (Bird) and being destroyed after leaving the game scene

---
+++ scripts/obstacle.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var speed := -215.0
+
+func _physics_process(delta: float) -> void:
+	velocity.x = speed
+	move_and_slide()
+	
+	if global_position.x < -200.0:
+		queue_free()
+
+func _on_pipe_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("something")
+
+
+func _on_score_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("nothing")
